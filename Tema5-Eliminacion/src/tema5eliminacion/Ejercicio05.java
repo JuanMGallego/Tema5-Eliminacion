c97ff6d9d8413ec64cdd7a10fe567bc9c3b2cb9c
jmgallego <jmgallego@106-09.info2>
2023-02-01 14:42:22 +0100

    Exercise 5 unfinished

--- Tema5-Eliminacion/src/tema5eliminacion/Ejercicio05.java
+++ Tema5-Eliminacion/src/tema5eliminacion/Ejercicio05.java
@@ -1,18 +1,55 @@
 package tema5eliminacion;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
 		
-		int favNum[] = new int[0];
+		int favNum[] = new int[1];
 		
-		while ()
+		int num1, num2;
+		int count = 0;
 		
-		while ()
+		System.out.println("Introduzca sus números favoritos (negativo para terminar): ");
+		
+		while (favNum[count] >= 0) {
+			
+			favNum[count] = sc.nextInt();
+			
+			if (favNum[count] < 0) {
+				
+				favNum = Arrays.copyOf(favNum, favNum.length - 1);
+				
+				break;
+				
+			} else {
+				
+				count++;
+				
+				favNum = Arrays.copyOf(favNum, favNum.length + 1);
+				
+			}
+			
+		}
+		
+		while (favNum.length > 1) {
+			
+			num1 = favNum[(int)(Math.random()*favNum.length)];
+			num2 = favNum[(int)(Math.random()*favNum.length)];
+			
+			favNum[num1] = (num1 + num2) / 2;
+			
+			System.arraycopy();
+			
+		}
+		
+		System.out.println("Su número de la suerte es: " + favNum[0]);
+		
+		sc.close();
 
 	}
 
